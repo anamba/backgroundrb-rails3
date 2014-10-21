be213066e6907cf25c5c9eb48abd6f0bfa3b8666
Aaron Namba <aaron@biggerbird.com>
2014-10-21 13:46:36 +0900

    fixes to allow multiple servers to share the same pid file directory

--- lib/backgroundrb/bdrb_start_stop.rb
+++ lib/backgroundrb/bdrb_start_stop.rb
@@ -7,9 +7,9 @@ module BackgrounDRb
         puts "BackgrounDRb Already Running"
         exit(0)
       elsif dead? # dead, but pid exists
-        remove_pidfiles
+        File.rm(PID_FILE)
       end
-
+      
       # status == 3, not running.
       $stdout.sync = true
       print "Starting BackgrounDRb.... "
@@ -18,10 +18,14 @@ module BackgrounDRb
       puts "Done!"
       exit(0)
     end
-
+    
     def stop
-      pid_files = Dir["#{RAILS_HOME}/tmp/pids/backgroundrb_*.pid"]
-      puts "BackgrounDRb Not Running" if pid_files.empty?
+      if running?
+        kill_process(PID_FILE)
+      else
+        puts "BackgrounDRb Not Running"
+      end
+      
       pid_files.each do |x|
         begin
           kill_process(x)
@@ -29,29 +33,23 @@ module BackgrounDRb
           # stopping an already stopped process is considered a success (exit status 0)
         end
       end
-      remove_pidfiles
+      File.rm(PID_FILE) if pidfile_exists?
     end
-
+    
     # returns the correct lsb code for the status:
     # 0 program is running or service is OK
-    # 1 program is dead and /var/run pid file exists
+    # 1 program is dead and pid file exists
     # 3 program is not running
     def status
-      @status = begin
-                    if pidfile_exists? and process_running?
-                      0
-                    elsif pidfile_exists? # but not process_running
-                      1
-                    else
-                      3
-                    end
-                  end
-
-      return @status
+      case
+        when pidfile_exists? && process_running? then 0
+        when pidfile_exists?                     then 1
+        else                                          3
+      end
     end
-
-    def pidfile_exists?; File.exists?(PID_FILE); end
-
+    
+    def pidfile_exists? ; File.exists?(PID_FILE) ; end
+    
     def process_running?
       begin
         Process.kill(0,self.pid)
@@ -60,21 +58,16 @@ module BackgrounDRb
         false
       end
     end
-
-    def running?;status == 0;end
+    
+    def running? ; status == 0 ; end
+    
     # pidfile exists but process isn't running
-
-    def dead?;status == 1;end
-
+    def dead? ; status == 1 ; end
+    
     def pid
       File.read(PID_FILE).strip.to_i if pidfile_exists?
     end
-
-    def remove_pidfiles
-      require 'fileutils'
-      FileUtils.rm_r(Dir["#{RAILS_HOME}/tmp/pids/backgroundrb_*.pid"])
-    end
-
+    
     def start_bdrb
       require "rubygems"
       require "yaml"
@@ -82,37 +75,37 @@ module BackgrounDRb
       require "logger"
       require "packet"
       require "optparse"
-
+      
       require "bdrb_config"
       require RAILS_HOME + "/config/boot"
       require "active_support"
-
+      
       BackgrounDRb::Config.parse_cmd_options ARGV
-
+      
       require RAILS_HOME + "/config/environment"
       require "bdrb_job_queue"
       require "backgroundrb_server"
-
+      
       #proper way to daemonize - double fork to ensure parent can have no interest in the grandchild
       if fork                     # Parent exits, child continues.
         sleep(1)
       else
         Process.setsid                   # Become session leader.
-
+        
         op = File.open(PID_FILE, "w")
         op.write(Process.pid().to_s)
         op.close
-
+        
         if BDRB_CONFIG[:backgroundrb][:log].nil? or BDRB_CONFIG[:backgroundrb][:log] != 'foreground'
           redirect_io(SERVER_LOGGER)
         end
         $0 = "backgroundrb master"
         BackgrounDRb::MasterProxy.new()
       end
-
+      
       #File.open(PID_FILE, "w") {|pidfile| pidfile.write(main_pid)}
     end
-
+    
     def kill_process(pid_file)
       pid = File.open(pid_file, "r") { |pid_handle| pid_handle.gets.strip.to_i }
       pgid =  Process.getpgid(pid)
@@ -120,14 +113,14 @@ module BackgrounDRb
       File.delete(pid_file) if File.exists?(pid_file)
       puts "Stopped BackgrounDRb worker with pid #{pid}"
     end
-
-
+    
+    
     # Free file descriptors and
     # point them somewhere sensible
     # STDOUT/STDERR should go to a logfile
     def redirect_io(logfile_name)
       begin; STDIN.reopen "/dev/null"; rescue ::Exception; end
-
+      
       if logfile_name
         begin
           STDOUT.reopen logfile_name, "a"
@@ -138,7 +131,7 @@ module BackgrounDRb
       else
         begin; STDOUT.reopen "/dev/null"; rescue ::Exception; end
       end
-
+      
       begin; STDERR.reopen STDOUT; rescue ::Exception; end
       STDERR.sync = true
     end
